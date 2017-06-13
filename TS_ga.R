4e75ec4735a3b1b8bf013380ebb1556137caf28d
lilyi <lilyli@qnap.com>
2017-06-13 17:59:36 +0800

    update

--- TS_ga.R
+++ TS_ga.R
@@ -24,8 +24,21 @@ plot(tsdata[,6], type= "b")
 # y (去除季節指數的數列)
 tsdata[,7]=tsdata[,3]-tsdata[,6]
 plot(tsdata[,7], type= "b")
+
+for(i in 1:120){
+  if(tsdata[i,2] <= 5){
+    tsdata[i,11] <- 0
+  }else{
+    tsdata[i,11] <- 1
+  }
+}
+
+
 # x=lm(y~t)+S
-reg=lm(tsdata[,7]~tsdata[,1])
+reg=lm(tsdata[,7]~tsdata[,1] + tsdata[,11])
+
+
+
 tsdata[,8]=reg$fitted.values+tsdata[,6]
 plot(reg$fitted.values, type= "b")
 plot(tsdata[,8], type= "b")
@@ -33,4 +46,60 @@ plot(tsdata[,8], type= "b")
 tsdata[,9]=tsdata[,3]-tsdata[,8]
 # squared residual value
 tsdata[,10]=tsdata[,9]^2 
+
 write.csv(tsdata, "C:/Users/Lily/Documents/TS/ana.csv")
+###############################################
+mydata=read.csv("C:/Users/Lily/Documents/TS/Taiwan_2.csv",header=T) 
+tsdata=matrix(0, 120, 12)
+tsdata[,1]=mydata[,1]
+tsdata[,2]=mydata[,2]
+tsdata[,3]=mydata[,3]
+tsdata[,4]=mydata[,4]
+colnames(tsdata)=c("time", "year", "season", "x", "CMA", "r", "S", "y", "TR", "CI", "C", "I")
+CMA=numeric(114) 
+for(i in 1:114)
+{ 
+  CMA[i]=(tsdata[i,4]+2*tsdata[i+1,4]+tsdata[i+2,4]+tsdata[i+3,4]+tsdata[i+4,4]+tsdata[i+5,4]+tsdata[i+6,4])/7
+}
+# plotting a smooth curve
+plot(CMA, type= "b")
+tsdata[,5]=c(NA, NA, NA, CMA, NA, NA, NA)
+tsdata[,6]=tsdata[,4]/tsdata[,5]
+# seasonal index
+rm=tapply(tsdata[,6], list( tsdata[,2], tsdata[,3]), mean, na.rm =T) ; rm
+trm=apply(rm, 2, mean, na.rm =T) 
+index=trm/mean(trm) ; index
+# seasonal index
+tsdata[,7]=c(index[3:7], rep(index, 16), index[1:3])
+plot(tsdata[,7], type= "b")
+# tsdata[,7]=rep(index, 10)
+# plot(tsdata[,7], type= "b")
+# y (去除季節指數的數列)
+
+tsdata[,8]=tsdata[,4]/tsdata[,7]
+# x=lm(y~t)+S
+reg=lm(tsdata[,8]~tsdata[,1]) #+ tsdata[,11])
+
+# reg=lm(tsdata[,8]~tsdata[,1])
+plot(tsdata[,8], type= "b")
+lines(reg$fitted.values, type= "l")
+#計算去除季節指數後的數列 y 與 TR 之比值 CI
+tsdata[,9]=reg$fitted.values
+tsdata[,10]=tsdata[,8]/tsdata[,9]
+# 計算 C
+tsdata[,11]=NA
+for (i in 1:114)
+{
+  C=(tsdata[i,10]+ tsdata[i+1,10]+ tsdata[i+2,10]+ tsdata[i+3,10]+ tsdata[i+4,10]+ tsdata[i+5,10]+ tsdata[i+6,10])/7
+  tsdata[i+3, 11]=C
+}
+plot(tsdata[,11], type= "b")
+# 計算 I 
+tsdata[,12]=tsdata[,4]/(tsdata[,9]*tsdata[,7]*tsdata[,11])
+plot(tsdata[,12], type= "b") 
+
+
+Q1adj_std <- round(sd(tsdata[,8]),4)
+Q1adj_mean <- round(mean(tsdata[,8]),2)
+Q1adj_U <- round((Q1adj_mean + 2*Q1adj_std),2)
+Q1adj_L <- round((Q1adj_mean - 2*Q1adj_std),2)
--- example.R
+++ example.R
@@ -33,3 +33,49 @@ plot(tsdata[,8], type= "b")
 tsdata[,9]=tsdata[,3]-tsdata[,8]
 # squared residual value
 tsdata[,10]=tsdata[,9]^2 
+
+
+###############################################
+mydata=read.csv("C:/Users/Lily/Documents/TS/example.csv",header=T) 
+tsdata=matrix(0, 21, 12)
+tsdata[,1]=mydata[,1]
+tsdata[,2]=mydata[,2]
+tsdata[,3]=mydata[,3]
+tsdata[,4]=mydata[,4]
+colnames(tsdata)=c("time", "year", "season", "x", "CMA", "r", "S", "y", "TR", "CI", "C", "I")
+CMA=numeric(36)
+for(i in 1:36)
+{
+  CMA[i]=(tsdata[i,4]+2*tsdata[i+1,4]+2*tsdata[i+2,4]+2*tsdata[i+3,4]+tsdata[i+4,4])/8
+}
+# plotting a smooth curve
+plot(CMA, type= "b")
+tsdata[,5]=c(NA, NA, CMA, NA, NA)
+tsdata[,6]=tsdata[,4]/tsdata[,5]
+# seasonal index
+rm=tapply(tsdata[,6], list( tsdata[,2], tsdata[,3]), mean, na.rm =T) ; rm
+trm=apply(rm, 2, mean, na.rm =T) 
+index=trm/mean(trm) ; index
+# seasonal index
+tsdata[,7]=rep(index, 10)
+plot(tsdata[,7], type= "b")
+# y (去除季節指數的數列)
+tsdata[,8]=tsdata[,4]/tsdata[,7]
+# x=lm(y~t)+S
+reg=lm(tsdata[,8]~tsdata[,1])
+plot(tsdata[,8], type= "b")
+lines(reg$fitted.values, type= "l")
+#計算去除季節指數後的數列 y 與 TR 之比值 CI
+tsdata[,9]=reg$fitted.values
+tsdata[,10]=tsdata[,8]/tsdata[,9]
+# 計算 C
+tsdata[,11]=NA
+for (i in 1:36)
+{
+  C=(tsdata[i,10]+ tsdata[i+1,10]+ tsdata[i+2,10]+ tsdata[i+3,10]+ tsdata[i+4,10])/5
+  tsdata[i+2, 11]=C
+}
+plot(tsdata[,11], type= "b")
+# 計算 I 
+tsdata[,12]=tsdata[,4]/(tsdata[,9]*tsdata[,7]*tsdata[,11])
+plot(tsdata[,12], type= "b") 
